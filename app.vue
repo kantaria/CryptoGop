6a732372fa4f4b2ded293c551004618669b44b13
kantaria <kantariamo@yandex.ru>
2024-08-10 03:43:41 +0700

    Data Table

--- app.vue
+++ app.vue
@@ -1,10 +1,9 @@
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
-  </div>
+  <v-app>
+    <DataTable />
+  </v-app>
 </template>
 
 <script setup>
-import 'vuetify/styles'
+import DataTable from '~/components/DataTable.vue'
 </script>
